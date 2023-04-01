0c308a14dd2aeea20dfb720b1345fa4682adf5fc
James <james.ivie@gmail.com>
2023-04-01 15:43:50 -0600

    restrict output to a single line in success case, add help output

--- Program.cs
+++ Program.cs
@@ -3,12 +3,23 @@ using System.Text.RegularExpressions;
 
 Regex RegionsRegex = new(@"(\s*#region\s+)([^\r\n]+)((.|\r|\n)+?(?=#endregion))", RegexOptions.Compiled);
 
+if (args.Length > 1 && (args[1].StartsWith('-') || args[1].StartsWith('/')) && (args[1].TrimStart('-', '/').ToLowerInvariant() == "help" || args[1].TrimStart('-', '/') == "?"))
+{
+    Console.WriteLine("dotnet-markdown-sample-code usage:");
+    Console.WriteLine("  dotnet-markdown-sample-code [source [dest [language [timeout]]]]");
+    Console.WriteLine("    source: the relative or full path to the source code from which samples should be extracted (default is Samples.cs)");
+    Console.WriteLine("      dest: the relative or full path to the destination markdown file into which the sample code should be placed (default is README.md)");
+    Console.WriteLine("  language: the markdown language specifier so that markdown can colorize the code sample properly (default is csharp)");
+    Console.WriteLine("   timeout: the maximum number of seconds to keep trying to update the destination file (uses expontential backoff with random offset starting at 250ms, default is 30)");
+    Console.WriteLine("");
+    return;
+}
 string sourceFile = (args.Length > 1 ? args[1] : "Samples.cs");
 string targetFile = (args.Length > 2 ? args[2] : "README.md");
 string markDownLanguageSpecifier = (args.Length > 3 ? args[3] : "csharp");
 string timeout = (args.Length > 4 ? args[4] : "");
 int timeoutSeconds = string.IsNullOrEmpty(timeout) ? 30 : int.Parse(timeout);
-Console.WriteLine($"Updating {targetFile} from {sourceFile}...");
+Console.Write($"Updating {targetFile} from {sourceFile}...");
 string baseFolder = Directory.GetCurrentDirectory();
 string sourceFileFullPath = Path.Combine(baseFolder, sourceFile);
 string targetFileFullPath = Path.Combine(baseFolder, targetFile);
@@ -31,7 +42,7 @@ do
         }
         targetFileContents = targetFileContents.TrimEnd();
         File.WriteAllText(targetFileFullPath, targetFileContents);
-        Console.WriteLine($"Updated {targetFile} from {sourceFile}!");
+        Console.WriteLine($"Success!");
         break;
     }
     catch (Exception ex)
@@ -39,36 +50,12 @@ do
         // timeout?
         if ((DateTime.UtcNow - startTime) > TimeSpan.FromSeconds(timeoutSeconds))
         {
-            Console.WriteLine($"Timeout updating {targetFile} from {sourceFile}: {ex.Message}");
+            Console.WriteLine($"{Environment.NewLine}Timeout updating {targetFile} from {sourceFile}: {ex.Message}");
             break;
         }
-        else if (attempt > 2) Console.WriteLine($"Error updating {targetFile} from {sourceFile}: {ex.Message}\r\nRetrying in a bit...");
-        System.Threading.Thread.Sleep((1 << attempt) * 250 + (int)(TickBasedRandom.GetRandom() % 250));
+        int waitMs = (1 << attempt) * 250 + (int)(TickBasedRandom.GetRandom() % 250);
+        if (attempt > 2) Console.Write($"{Environment.NewLine}Error updating {targetFile} from {sourceFile}: {ex.Message}\r\nRetrying in {waitMs}ms...");
+        System.Threading.Thread.Sleep(waitMs);
         // fall through and retry
     }
 } while (true);
-/* Source Powershell:
-# NOTE: you may have to run this the first time you build:
-# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
-"Updating README.md from Samples.cs..."
-$scriptPath=If(!$PSCommandPath) { (Get - Location).Path }
-Else { Split-Path -parent $PSCommandPath }
-$samplesFile=$scriptPath+"\AmbientServicesSamples\Samples.cs"
-$readmeFile=$scriptPath+"\README.md"
-$samplesFileContents=Get-Content $samplesFile -Raw
-$samplesRegionPattern="[\r\n]\s*#region\s+([^\r\n\s]*)(.|\r|\n)+?(?=#endregion)"
-$rawRegions=[Regex]::Matches($samplesFileContents,"(\s*#region\s+)([^\r\n]+)((.|\r|\n)+?(?=#endregion))")
-$readmeFileContents=Get-Content $readmeFile -Raw
-ForEach($_ in $rawRegions)
-{ 
-    $regionName =$_.Groups[2].Value
-    $regionContents =$_.Groups[3].Value.Trim()
-    $sampleInsert = "[//]: # (" + $regionName + ")`r`n``````csharp`r`n" + $regionContents + "`r`n``````"
-    $readmeReplacePattern = "\[\/\/\]\:\s*\#\s*\(" + $regionName + "\).*[\r\n]``````[^\r\n]*(.|\r|\n)+?(?=``````)``````"
-    $readmeFileContents =[Regex]::Replace($readmeFileContents,$readmeReplacePattern,$sampleInsert)
-    #($readmeFileContents -Replace $readmeReplacePattern,$sampleInsert)
-}
-$readmeFileContents=$readmeFileContents.TrimEnd();
-Set-Content -Path $readmeFile -Value $readmeFileContents
-"Updated README.md from Samples.cs!"
-*/
